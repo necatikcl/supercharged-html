0cbcd3d0159cff42c7d439caa5f79668f11e7e5a
Necati <necatifk07@gmail.com>
2022-07-17 15:47:24 +0300

    feat: add `s-if` support

--- utils/renderComponent.js
+++ utils/renderComponent.js
@@ -1,8 +1,37 @@
 const compileSlots = require('./compileSlots')
 const inheritAttributes = require('./inheritAttributes');
-const renderChildComponents = require('./renderChildComponents')
+
+const removeConditionalElements = (componentPlaceholder, componentSource) => {
+  const conditionalElements = componentSource.querySelectorAll('[s-if]');
+
+  conditionalElements.forEach(conditionalElement => {
+    const nextElement = conditionalElement.nextElementSibling;
+    const conditionalVariable = conditionalElement.getAttribute('s-if');
+    const placeholderAttr = componentPlaceholder.getAttribute(conditionalVariable);
+
+    if (!conditionalVariable) return;
+
+    componentPlaceholder.removeAttribute(conditionalVariable);
+
+    if (placeholderAttr === undefined || placeholderAttr === "false") {
+      conditionalElement.remove();
+
+      if (nextElement.hasAttribute('s-else')) {
+        nextElement.removeAttribute('s-else');
+      }
+    } else {
+      conditionalElement.removeAttribute('s-if');
+
+      if (nextElement.hasAttribute('s-else')) {
+        nextElement.remove()
+      }
+    }
+  }
+  )
+}
 
 const renderComponent = (componentPlaceholder, componentSource) => {
+  removeConditionalElements(componentPlaceholder, componentSource);
   compileSlots(componentPlaceholder, componentSource);
 
   const newElement = inheritAttributes(componentSource, componentPlaceholder);
