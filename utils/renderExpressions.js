cc4ff1bef41021a08e36b23bab1bd46e6606c5e1
Necati <necatifk07@gmail.com>
2022-09-07 16:54:51 +0300

    fix

--- utils/renderExpressions.js
+++ utils/renderExpressions.js
@@ -3,10 +3,9 @@ const checkPropsUsage = require('./checkPropsUsage')
 
 const renderExpressions = (htmlString, props) => {
   const expressions = htmlString.match(/{{(.*?)}}/g)
-    ?.map((val) => val.replace(/{{\s*|\s*}}/g, ''));
+    ?.map((val) => val.replace(/{{\s*|\s*}}/g, '')) || [];
 
   checkPropsUsage(htmlString, props);
-
 
   expressions.forEach(expression => {
     const expressionValue = evalWithProps(expression, props);
